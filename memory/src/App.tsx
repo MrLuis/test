4dffff59e20e0f2b5b97cef194204e4f5691f412
Developer <luisrortega@yahoo.com>
2022-11-16 14:20:43 -0500

    Added random generator

--- memory/src/App.tsx
+++ memory/src/App.tsx
@@ -6,26 +6,29 @@ function App() {
 
   type card ={
     value : number,
-    revealed: boolean
+    revealed: boolean,
+    found : boolean | undefined,
+    color : string 
   }
 
   const [cards, setCards] = useState<Array<card>>([]);
 
+  const [prevCard, setPrevCard] = useState<card | undefined>();
 
   function initCards(){
     setCards([
-      {value:1,revealed: false},
-      {value:3,revealed: false},
-      {value:5,revealed: false},
-      {value:2,revealed: false},
-      {value:5,revealed: false},
-      {value:3,revealed: false},
-      {value:4,revealed: false},
-      {value:1,revealed: false},
-      {value:6,revealed: false},
-      {value:2,revealed: false},
-      {value:4,revealed: false},
-      {value:6,revealed: false},
+      {value:1,revealed: false, found :false, color: 'grey'},
+      {value:3,revealed: false, found :false, color: 'grey'},
+      {value:5,revealed: false, found :false, color: 'grey'},
+      {value:5,revealed: false, found :false, color: 'grey'},
+      {value:3,revealed: false, found :false, color: 'grey'},
+      {value:2,revealed: false, found :false, color: 'grey'},
+      {value:4,revealed: false, found :false, color: 'grey'},
+      {value:1,revealed: false, found :false, color: 'grey'},
+      {value:6,revealed: false, found :false, color: 'grey'},
+      {value:2,revealed: false, found :false, color: 'grey'},
+      {value:4,revealed: false, found :false, color: 'grey'},
+      {value:6,revealed: false, found :false, color: 'grey'},
     ])
   }
 
@@ -34,9 +37,54 @@ function App() {
   },[]);
 
   const handleClick  = (index:number)=> {
-    const curr = [...cards];
-    curr[index].revealed = true;
-    setCards(curr);
+    console.log('clicked',index);
+    const currCards = [...cards];
+    const currCard = currCards[index];
+    if (currCard.found){
+      return;
+    }
+  
+    if (!prevCard){// no card currently selected
+
+      currCard.revealed = true;
+      setCards(currCards);
+      setPrevCard(currCard);
+    }
+    else {
+      if (prevCard.value === currCard.value ){
+        prevCard.found = true;
+        currCard.revealed = true;
+        currCard.found = true;
+        currCard.color = 'green';
+        prevCard.color = 'green';
+        setCards(currCards);
+        setPrevCard(undefined);
+
+        if (currCards.filter((item:card)=> {return item.found === false}).length === 0 ){
+          setTimeout(()=>{
+            alert('Congrats!!!');
+            initCards()
+
+          });
+
+        }
+      }
+      else {
+        currCard.color = 'red';
+        prevCard.color = 'red';
+        currCard.revealed = true;
+        setCards(currCards);
+        setTimeout(()=>{
+
+          currCard.color = 'grey';
+          prevCard.color = 'grey';
+          prevCard.revealed = false;
+          currCard.revealed = false;
+          setCards(currCards);
+          setPrevCard(undefined);
+        },1000)
+      }
+    }
   }
 
   return (
@@ -45,7 +93,7 @@ function App() {
       <div className="deck">
         {
           cards.map(function (item:card, index:number) {
-            return <div className="card" key={index} onClick={() =>{handleClick(index)}} >{(item.revealed?item.value:'?')}</div>
+            return <div style={{borderColor:item.color, color:item.color}} className="card" key={index} onClick={() =>{handleClick(index)}} >{(item.revealed?item.value:'?')}</div>
           })
         }
       </div>
